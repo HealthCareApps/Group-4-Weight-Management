8e2944052a0cdb50484645bdd9d81859bcbe60da
Andrew <abarbos3@fau.edu>
2016-04-01 13:28:15 -0400

    Added the userObject

---
+++ ScaleFIt/app/src/main/java/www/team4/com/scalefit/userObject.java
@@ -0,0 +1,27 @@
+package www.team4.com.scalefit;
+
+/**
+ * Created by Andrew on 4/1/2016.
+ */
+
+
+public class userObject {
+
+    private String firstName;
+    private String lastName;
+    private String userEmail;
+    private String userPassword;
+    private int age;
+    private int DOB;
+    private int weight;
+    private int BMI;
+    private float height;
+    private float armMeasure;
+    private float thighMeasure;
+    private float chestMeasure;
+    private float waistMeasure;
+    private float neckMeasure;
+    private float hipMeasure;
+
+
+}
