e460080b6d0e9ddba26db4df8171838351c0e7a5
Alexander Joiseus <ajoiseus@fau.edu>
2016-04-25 16:57:37 -0400

    Updated Project Assignment 2 Folder for grading

---
+++ Project Assignment 2/ScaleFIt/app/src/main/java/www/team4/com/scalefit/MainActivity.java
@@ -0,0 +1,124 @@
+package www.team4.com.scalefit;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static  final boolean On = true;
+    private static  final boolean Off = false;
+    private final static String TAG = Activity.class.getSimpleName();
+
+    String TITLES[] = {"Home","Profile"};
+    int ICONS[] = {R.drawable.ic_account_balance_black_24dp,R.drawable.ic_person_black_24dp};
+
+    String NAME = "Andrew Barbosa";
+    String EMAIL = "abab@gmail.com";
+    int PROFILE = R.drawable.andrew2;
+    private Toolbar toolbar;
+    RecyclerView mRecyclerView;
+    RecyclerView.Adapter mAdapter;
+    RecyclerView.LayoutManager mLayoutManager;
+    DrawerLayout Drawer;
+    ActionBarDrawerToggle mDrawerToggle;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if (On) Log.i(TAG, "onCreate Called");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         toolbar = (Toolbar) findViewById(R.id.app_bar);
+        if (On) Log.i(TAG, "Toolbar Created");
+        setSupportActionBar(toolbar);
+        mRecyclerView = (RecyclerView) findViewById(R.id.RecyclerView);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mAdapter = new MyAdapter1(TITLES,ICONS,NAME,EMAIL,PROFILE);
+
+
+        mRecyclerView.setAdapter(mAdapter);
+
+        mLayoutManager = new LinearLayoutManager(this);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        Drawer = (DrawerLayout) findViewById(R.id.DrawerLayout);
+        mDrawerToggle = new ActionBarDrawerToggle(this,Drawer,toolbar,R.string.openDrawer,
+                R.string.closeDrawer) {
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                // Code here will execute once drawer is closed
+            }
+        };  Drawer.setDrawerListener(mDrawerToggle);
+            mDrawerToggle.syncState();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        if (On) Log.i(TAG, " onCreateOptionsMenu Called");
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        if (On) Log.i(TAG, "onOptionItemSelected");
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+        if (id == R.id.action_settings) {
+            if (On) Log.i(TAG, "Action Settings Selected");
+            startActivity(new Intent(this, Settings.class));
+            Toast.makeText(this, "Hey you just hit " + item.getTitle(), Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if (id == R.id.action_about) {
+            if (On) Log.i(TAG, "Action About Selected");
+            startActivity(new Intent(this, aboutActivity.class));
+            Toast.makeText(this, "Hey you just hit " + item.getTitle(), Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if (id == R.id.action_team) {
+            if (On) Log.i(TAG, "Action Team Option Selected");
+            startActivity(new Intent(this, Team4Activity.class));
+            Toast.makeText(this, "Hey you just hit " + item.getTitle(), Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if (id == R.id.action_weight) {
+            if (On) Log.i(TAG, "Action Weight Option Selected");
+            startActivity(new Intent(this, weightScreenActivity.class));
+            Toast.makeText(this, "Hey you just hit " + item.getTitle(), Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
+
+
---
+++ Project Assignment 2/ScaleFIt/app/src/main/java/www/team4/com/scalefit/team4.java
@@ -0,0 +1,36 @@
+package www.team4.com.scalefit;
+
+/**
+ * Created by Reaper on 3/9/2016.
+ */
+public class team4 {
+    static String[] DetailList = {
+            "Alexander Joiseus",
+            "Richard Karl",
+            "Katherine Arreaza",
+            "Demetrius Dukes",
+            "Andrew Barbosa"
+    };
+
+    static String[] Details = {
+            "Alexander Joiseus\n\n Lead Application Developer. Senior in Computer Science & " +
+                    "Computer Engineering",
+
+            "Richard Karl\n\n A junior working towards Bachelor's in Computer\n" +
+                    "Engineering. I enjoy the challenge of programming (currently know C,\n" +
+                    "C++, Assembly, and working on Java and XML from Software/Hardware),\n" +
+                    "playing sudoku (mainly the harder difficulties) , playing the drums\n" +
+                    "(I'm self taught and play by ear and figured out how to play by\n" +
+                    "listening to the beats and grooves in songs) and video games in my\n" +
+                    "spare time.",
+
+            "Katherine Arreaza\n\n I am a graduate Nursing student , working towards " +
+                    "becoming a Nurse Practioner. I currently work full-time as a trauma intensive "
+                    + "care nurse. My hobbies include working out and just spending time with " +
+                    "family and friends. I also like going to the beach and going to the movies.",
+
+            "Demetrius Dukes\n\n Lead Graphical Artist",
+
+            "Andrew Barbosa \n\n Application Developer, great programmer. Overall Really Cool guy"
+    };
+}
